57e0fdb0891fc1bcaa97f634ef6f705c77a92a6b
Chris <morenochristopher580@gmail.com>
2025-07-18 18:06:11 -0500

    Create shoo.js

---
+++ shoo.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    console.log(`why u in devtools..?`);
+    console.log(`why u in devtools..?`);
+    console.log(`why u in devtools..?`);
+    console.log(`why u in devtools..?`);
+    console.log(`why u in devtools..?`);
+    console.log(`why u in devtools..?`);
+    console.log(`why u in devtools..?`);
+    console.log(`why u in devtools..?`);
+    console.log(`scroll up ig for more`);
+  
+    
+};
